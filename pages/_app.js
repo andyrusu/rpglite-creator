f3538c1ce9589d0e824d63a5de2883fb6a292cfc
Andi Rusu <rusu.andy1@gmail.com>
2021-10-22 17:17:37 +0300

    Moved config to .env file

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,14 @@ import { initializeApp } from "firebase/app";
 import { ProvideAuth } from "../components/provideAuth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAD7Eq6hUEVGuKMRTUWH5CYyisyqP-aQ64",
-  authDomain: "rpglite-3fc46.firebaseapp.com",
-  projectId: "rpglite-3fc46",
-  databaseURL:
-    "https://rpglite-3fc46-default-rtdb.europe-west1.firebasedatabase.app/",
-  storageBucket: "rpglite-3fc46.appspot.com",
-  messagingSenderId: "767985415267",
-  appId: "1:767985415267:web:7f599cee190f8e8e953456",
-  measurementId: "G-EJ8B18N0P1",
+  apiKey: process.env.FIREBASE_APPKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
